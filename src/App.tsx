afb49237fade23bc95a657be2199f880caf50a61
lpozo1990 <lpozo1990@gmail.com>
2023-12-29 13:08:08 -0500

    Add tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import "./App.css";
 
 function App() {
-  return <>Hola mundo</>;
+  return (
+    <>
+      <div className="text-red-800 text-2xl w-full bg-slate-300 text-center py-8 font-bold">
+        Login page
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}", // Include your React components
+    "./public/index.html", // Include your HTML file
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
