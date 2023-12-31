124d88a92121303c6cef5fbb401cc83190e8c381
lpozo1990 <lpozo1990@gmail.com>
2023-12-31 12:27:59 -0500

    Fix typo

--- src/components/Products/index.tsx
+++ src/components/Products/index.tsx
@@ -42,7 +42,7 @@ const Index: FunctionComponent<IndexProps> = () => {
             className="block text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
             type="button"
           >
-            Crear Productos
+            Crear Producto
           </button>
         </div>
       </div>
